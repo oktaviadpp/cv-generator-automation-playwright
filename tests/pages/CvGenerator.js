73aa86082bc196d52c5b6efbcdbdbd67cc7d2333
oktaviadwiputripermatasari <oktaviadwiputri506@gmail.com>
2026-01-19 22:48:02 +0700

    add:locator generator

---
+++ tests/pages/CvGenerator.js
@@ -0,0 +1,35 @@
+export class CvGenerator {
+    constructor (page){
+        this.page = page;
+        this.buttonNext = page.getByRole("button", {name:"Next"});
+
+        //Personal Info
+        this.name = page.locator("#name");
+        this.email = page.locator("#email");
+        this.phone = page.locator("#phone");
+        this.location = page.locator("#location");
+        this.linkedin = page.locator("#linkedin");
+        this.github = page.locator("#github");
+        this.portfolio = page.locator("#portfolio");
+        this.summary = page.locator("#summary");
+        this.languages = page.locator("#languages");
+        this.buttonMenuBuildYourCV = page.getByRole("link", {name:"Build Your CV"});
+
+        //Experience
+        
+    }
+
+    async BuildYourCV(){
+        await this.buttonMenuBuildYourCV.click();
+        await this.name.fill("Oktavia Dwi Putri Permatasari");
+        await this.email.fill("oktaviadwiputri506@gmail.com");
+        await this.phone.fill("082311439450");
+        await this.location.fill("Jakarta");
+        await this.linkedin.fill("https://www.linkedin.com/in/oktaviadpp/");
+        await this.github.fill("https://github.com/oktaviadpp");
+        await this.portfolio.fill("https://github.com/oktaviadpp");
+        await this.summary.fill("I am an Information System graduate and have over 1,5+ years of work experience Quality Assurance and Software Tester. Proficient in various testing methodologies, including manual and automation testing, with expertise in utilizing tools such as Katalon for automation. Skilled in API testing, performance testing, test case development, and bug reporting. Experience in preparing design documents for several government projects. Committed to continuous learning and improvement to stay updated with the latest technologies and industry best practices in Quality Assurance.");
+        await this.languages.fill("Indonesia");
+        await this.buttonNext.click();
+    }
+};
--- tests/web-api-skip-generate-code-tutorial.spec.js
+++ tests/web-api-skip-generate-code-tutorial.spec.js
@@ -1,21 +1,20 @@
 import {test, expect, request} from "@playwright/test";
-const {ApiUtils} = require('./utils/api-utils.spec');
+import {CvGenerator} from "./pages/CvGenerator.js";
+const {ApiUtils} = require('./utils/api-utils.js');
 
 test('01-generate cv without generate code and tutorial successfully', async ({page})=> {
+    //inject userCode to Local storage
     const apiContext = await request.newContext();
     const apiUtils = new ApiUtils(apiContext);
 
     const userCode = await apiUtils.getUserCode();
     await apiUtils.injectLocalStrorage(page, userCode);
-
-    await page.goto('https://cv.review.aforsy.my.id/');
-
-    //Generate CV Process
-    const buttonMenuBuildYourCV = page.getByRole("link", {name:"Build Your CV"})
-    const buttonNext = page.getByRole("button", {name:"Next"});
     
+    //Create CvGenerator object
+    const cvGenerator = new CvGenerator(page);
+    
+    //Generate CV Process
+    await page.goto('https://cv.review.aforsy.my.id/');
+    await cvGenerator.BuildYourCV();
 
-    await expect(buttonMenuBuildYourCV).toBeVisible();
-    await buttonMenuBuildYourCV.click();
-    await expect(buttonNext).toBeVisible();
 });
