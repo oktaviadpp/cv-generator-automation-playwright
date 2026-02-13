b19d816473cab8839c667b4ea66cb5116092151b
oktaviadpp <oktaviadwiputri506@gmail.com>
2026-02-13 19:06:58 +0700

    add

--- tests/CvGenerator-03-Verify-NewTab.spec.js
+++ tests/CvGenerator-03-Verify-NewTab.spec.js
@@ -2,7 +2,7 @@ import {test, expect, request} from "@playwright/test";
 import {CvGenerator} from "./pages/CvGenerator.js";
 const {ApiUtils} = require('./utils/api-utils.js');
 
-test('02-import template cv successfully', async ({page}, testInfo)=> {
+test('03-verify new tab when open preview', async ({page}, testInfo)=> {
     //inject userCode to Local storage
     const apiContext = await request.newContext();
     const apiUtils = new ApiUtils(apiContext);
